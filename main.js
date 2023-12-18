fbb1e989d794ecea21a157fd11646cd1b1c445b0
YamagataMiho <149132492+YamagataMiho@users.noreply.github.com>
2023-12-18 13:01:16 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -4,7 +4,7 @@
 $('.slider').slick({
     autoplay: true,//自動的に動き出すか。初期値はfalse。
     infinite: true,//スライドをループさせるかどうか。初期値はtrue。
-    speed: 500,//スライドのスピード。初期値は300。
+    speed: 700,//スライドのスピード。初期値は300。
     slidesToShow: 3,//スライドを画面に3枚見せる
     slidesToScroll: 1,//1回のスクロールで1枚の写真を移動して見せる
     prevArrow: '<div class="slick-prev"></div>',//矢印部分PreviewのHTMLを変更
@@ -16,27 +16,39 @@ $('.slider').slick({
 //   
 
 
+//スクロールした際の動きを関数でまとめる
+function PageTopAnime() {
+  var scroll = $(window).scrollTop();
+  if (scroll >= 200){//上から200pxスクロールしたら
+    $('#page-top').removeClass('RightMove');//#page-topについているRightMoveというクラス名を除く
+    $('#page-top').addClass('LeftMove');//#page-topについているLeftMoveというクラス名を付与
+  }else{
+    if(
+      $('#page-top').hasClass('LeftMove')){//すでに#page-topにLeftMoveというクラス名がついていたら
+      $('#page-top').removeClass('LeftMove');//LeftMoveというクラス名を除き
+      $('#page-top').addClass('RightMove');//RightMoveというクラス名を#page-topに付与
+    }
+  }
+}
 
-//上部画像の設定
-$('.gallery').slick({
-    infinite: true, //スライドをループさせるかどうか。初期値はtrue。
-    fade: true, //フェードの有効化
-    arrows: true,//左右の矢印あり
-    prevArrow: '<div class="slick-prev"></div>',//矢印部分PreviewのHTMLを変更
-    nextArrow: '<div class="slick-next"></div>',//矢印部分NextのHTMLを変更
-  });
-  
-  //選択画像の設定
-  $('.choice-btn').slick({
-    infinite: true, //スライドをループさせるかどうか。初期値はtrue。
-    slidesToShow: 8, //表示させるスライドの数
-    focusOnSelect: true, //フォーカスの有効化
-    asNavFor: '.gallery', //連動させるスライドショーのクラス名
-  });
-    
-  //下の選択画像をスライドさせずに連動して変更させる設定。
-  $('.gallery').on('beforeChange', function(event, slick, currentSlide, nextSlide) {
-    var index = nextSlide; //次のスライド番号
-    //サムネイルのslick-currentを削除し次のスライド要素にslick-currentを追加
-    $(".choice-btn .slick-slide").removeClass("slick-current").eq(index).addClass("slick-current");
-  });
+// 画面をスクロールをしたら動かしたい場合の記述
+$(window).scroll(function () {
+PageTopAnime();/* スクロールした際の動きの関数を呼ぶ*/
+});
+
+// ページが読み込まれたらすぐに動かしたい場合の記述
+$(window).on('load', function () {
+PageTopAnime();/* スクロールした際の動きの関数を呼ぶ*/
+});
+
+
+// #page-topをクリックした際の設定
+$('#page-top').click(function () {
+  $('body,html').animate({
+      scrollTop: 0//ページトップまでスクロール
+  }, 500);//ページトップスクロールの速さ。数字が大きいほど遅くなる
+  return false;//リンク自体の無効化
+});
+
+
+
