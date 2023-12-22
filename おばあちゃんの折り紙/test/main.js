1b69455e43e2bd0616672295b868d16f74d6bba5
YamagataMiho <149132492+YamagataMiho@users.noreply.github.com>
2023-12-23 00:21:42 +0900

    Add files via upload

---
+++ おばあちゃんの折り紙/test/main.js
@@ -0,0 +1,9 @@
+
+
+$(function () {
+    $('#js-hamburger-menu, .navigation__link').on('click', function () {
+      $('.navigation').slideToggle(500)
+      $('.hamburger-menu').toggleClass('hamburger-menu--open')
+    });
+  });
+  
